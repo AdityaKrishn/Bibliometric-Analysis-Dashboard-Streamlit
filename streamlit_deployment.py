fb94aaf3795e48790166d6c4de3feef72c36aaf4
adityakb95 <adityakb95@gmail.com>
2024-01-29 00:35:38 +0530

    Amended top keywords chart

--- streamlit_deployment.py
+++ streamlit_deployment.py
@@ -69,7 +69,8 @@ with st.container():
         st.altair_chart(alt.Chart(dict['keywords']).mark_bar().encode(x=alt.X('keyword', sort=None), y='count'), use_container_width=True)
     with col2:
         st.subheader("Papers published each year - All publications(combined view)")
-        st.bar_chart(dict['combined_db_grouped'], x = 'year', y = 'counts')
+        combined_db_grouped = dict['combined_db_grouped']
+        st.bar_chart(combined_db_grouped.head(15), x = 'year', y = 'counts')
 
 
 
